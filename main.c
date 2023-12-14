1753f88f15e4a1911179346189abc69b7b437034
rahma korish <rahmakorish@gmail.com>
2023-12-14 13:50:21 -0800

    betty

--- main.c
+++ main.c
@@ -1,51 +1,35 @@
 #include "monty.h"
-#include <stdio.h>
-#include <stdlib.h>
-#define  _GNU_SOURCE
-/*
-char *readline()
-{
-	char *line = NULL;
-	size_t len = 0;
-	int nread;
-
-	nread = getline(&line, &len, stdin);
-	if (nread == -1)
-	{
-		free(line);
-		return (NULL);
-	}
-
-	if (nread > 0 && line[nread - 1] == '\n')
-		line[nread - 1] = '\0';
-
-	return (line);
-}*/
+/**
+ *main-entry point for monty file interperator
+ *@argc:argument count
+ *@argv:argument vector
+ *Return:0success
+ **/
 int main(int argc, char **argv)
 {
 	FILE *montyfile;
-	char buffer[1024]; char* token;
+	char buffer[1024];
+	char *token;
 
 	if (argc < 2)
-	{		
+	{
 		perror("USAGE: monty file\n");
 		return (EXIT_FAILURE);
 	}
-	else 
+	else
 	{
-	montyfile = fopen(argv[1],"r");	
+	montyfile = fopen(argv[1], "r");
 	if (!montyfile)
 	{
 		perror("USAGE: monty file\n");
-                return (EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	}
-	while (fgets(buffer, sizeof(buffer),montyfile))
+	while (fgets(buffer, sizeof(buffer), montyfile))
 	{
-		token = strtok(buffer," ");
-			printf("%s\n",token);
+		token = strtok(buffer, " ");
+		printf("%s\n", token);
 	}
-	fclose(montyfile);	
+	fclose(montyfile);
 	return (EXIT_SUCCESS);
-
 	}
 }
