9f116937a9cb0f2f1265475097ba944c67dd8545
rahma korish <rahmakorish@gmail.com>
2023-12-14 08:52:33 -0800

    add1

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
-#include "main.h"
+#include "monty.h"
 #include <stdio.h>
 #include <stdlib.h>
 #define  _GNU_SOURCE
+/*
 char *readline()
 {
 	char *line = NULL;
@@ -11,22 +12,19 @@ char *readline()
 	nread = getline(&line, &len, stdin);
 	if (nread == -1)
 	{
-		/* Error or end of file */
 		free(line);
 		return (NULL);
 	}
 
-	/* Remove the trailing newline character, if it exists */
 	if (nread > 0 && line[nread - 1] == '\n')
 		line[nread - 1] = '\0';
 
 	return (line);
-}
+}*/
 int main(int argc, char **argv)
 {
-	/*FILE *montyfile;
-	char buffer[1024];*/
-	char *input;
+	FILE *montyfile;
+	char buffer[1024]; char* token;
 
 	if (argc < 2)
 	{		
@@ -34,16 +32,19 @@ int main(int argc, char **argv)
 		return (EXIT_FAILURE);
 	}
 	else 
-	{	printf("%s\n",argv[1]);
-		input = readline();
-		printf("input %s\n",input);
-/*char *text = argv[1];
-	montyfile = fopen(text,"r");	
+	{
+	montyfile = fopen(argv[1],"r");	
 	if (!montyfile)
 	{
 		perror("USAGE: monty file\n");
                 return (EXIT_FAILURE);
-	}*/	
+	}
+	while (fgets(buffer, sizeof(buffer),montyfile))
+	{
+		token = strtok(buffer," ");
+			printf("%s\n",token);
+	}
+	fclose(montyfile);	
 	return (EXIT_SUCCESS);
 
 	}
